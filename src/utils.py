d1dce2bd1d82490bf6265e4c84df24344eadcb3f
SATRIA HADI WIYANA <114901501+SatriaHadiWiyana7@users.noreply.github.com>
2025-11-21 22:07:52 +0700

    Feat: Create utils to Save and Load Model

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,25 @@
 import json
 import os
 import numpy as np
+
+def save_to_processed(data, filename):
+    """Simpan data (dictionary/list) ke folder data/processed sebagai JSON"""
+    os.makedirs("data/processed", exist_ok=True)
+    path = os.path.join("data/processed", filename)
+    
+    # Convert numpy types to python types for JSON serialization
+    def convert(o):
+        if isinstance(o, np.float32) or isinstance(o, np.float64): return float(o)
+        if isinstance(o, np.ndarray): return o.tolist()
+        raise TypeError
+        
+    with open(path, 'w') as f:
+        json.dump(data, f, default=convert, indent=4)
+    print(f"Data tersimpan di {path}")
+
+def load_from_processed(filename):
+    path = os.path.join("data/processed", filename)
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            return json.load(f)
+    return {}
